1e1a4c8f2d5bb0aea73f76ee7aacbf08961cb7ab
Edwanderson <edi171194@gmail.com>
2024-11-23 13:30:51 -0300

    Adicionando provider e removendo backend

--- terraform-azdo-modules/azuredevops_repository/provider.tf
+++ terraform-azdo-modules/azuredevops_repository/provider.tf
@@ -7,6 +7,9 @@ terraform {
       version = "1.4.0"
     }
   }
-  
-  backend "local" {}
+}
+
+provider "azuredevops" {
+  org_service_url       = var.org_service_url
+  personal_access_token = var.personal_access_token
 }
