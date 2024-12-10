f6f9fdfd067bd622e5bfd347ecd7aed95643630a
Edwanderson <edi171194@gmail.com>
2024-12-10 06:08:30 -0300

    Remoção TFVARS que foi criado para testes e remoção de variables não utilizadas.

---
+++ aws/terraform-aws-secretmanager/example/README.md
@@ -0,0 +1,5 @@
+Lembrete: 
+
+pre-requisitos.
+1. deve-ser configurado o aws configure no S.O para que seja coletado a autenticação da AWS. (pois no modulo é utilizado o provider aws)
+2. 
--- aws/terraform-aws-secretmanager/variables.tf
+++ aws/terraform-aws-secretmanager/variables.tf
@@ -7,16 +7,3 @@ variable "region" {
   description = "Região da AWS para os recursos"
   type        = string
 }
-
-variable "aws_profile" {
-  description = "Perfil AWS do CLI a ser usado"
-  type        = string
-  default     = "default"
-}
-
-# variable "secrets" {
-#   description = "Mapa de secrets e seus valores"
-#   default = {
-#     "azuredevops/terraform/" = "password123"
-#   }
-# }
